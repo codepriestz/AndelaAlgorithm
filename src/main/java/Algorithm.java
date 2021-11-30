b36f14f50bd2c3a2f5d8748b90e649343a2062a0
codepriest <codepriest.gudana@gmail.com>
2021-11-30 13:23:12 +0100

    add constraints

--- src/main/java/Algorithm.java
+++ src/main/java/Algorithm.java
@@ -1,23 +1,22 @@
-package com.andela;
-
-
-
 public class Algorithm {
 
 
   public static void main(String[] args) {
 
-    int[] input = new int[]{2,1,1,1,5,3,2};
+    int[] input = new int[]{2};
     System.out.println("Result: " + getMaxColors(input, 7));
   }
 
   public static int getMaxColors(int[] prices, int money) {
+    if (prices.length == 1 && prices[0] > money) {
+      return 0;
+    }
     int[] sum = new int[prices.length];
     sum[0] = prices[0];
     for (int i = 1; i < prices.length; i++) {
       sum[i] = sum[i - 1] + prices[i];
     }
-    int maxLength = Integer.MIN_VALUE;
+    int maxLength = 1;
     for (int i = prices.length - 1; i > 0; i--) {
       int curr = sum[i];
       for (int j = i - 1; j >= 0; j--) {
@@ -29,7 +28,5 @@ public class Algorithm {
     }
     return maxLength;
   }
-// [2,1,1,1,5,3,2]
-   [2,3,]
 
 }
