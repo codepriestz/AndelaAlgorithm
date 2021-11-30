f9f6701a832d3f49e50eb8251e403ad511393db6
codepriest <codepriest.gudana@gmail.com>
2021-11-30 13:07:23 +0100

    rename method

--- Algorithm.java
+++ Algorithm.java
@@ -8,21 +8,21 @@ public class Algorithm {
   public static void main(String[] args) {
 
     int[] input = new int[]{2,1,1,1,5,3,2};
-    System.out.println("Result: " + maximSubArray(input, 7));
+    System.out.println("Result: " + getMaxColors(input, 7));
   }
 
-  public static int maximSubArray(int[] painting, int price) {
-    int[] sum = new int[painting.length];
-    sum[0] = painting[0];
-    for (int i = 1; i < painting.length; i++) {
-      sum[i] = sum[i - 1] + painting[i];
+  public static int getMaxColors(int[] prices, int money) {
+    int[] sum = new int[prices.length];
+    sum[0] = prices[0];
+    for (int i = 1; i < prices.length; i++) {
+      sum[i] = sum[i - 1] + prices[i];
     }
     int maxLength = Integer.MIN_VALUE;
-    for (int i = painting.length - 1; i > 0; i--) {
+    for (int i = prices.length - 1; i > 0; i--) {
       int curr = sum[i];
       for (int j = i - 1; j >= 0; j--) {
         int diff = curr - sum[j];
-        if (diff <= price) {
+        if (diff <= money) {
           maxLength = Math.max(i - j, maxLength);
         }
       }
